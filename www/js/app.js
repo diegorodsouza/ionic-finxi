4afa192d286f2b16da8da0a4e4b2b296d36b04e4
Diego Souza <diegosouza@ejcm.com.br>
2016-10-20 16:18:01 -0200

    +camera gravando

--- www/js/app.js
+++ www/js/app.js
@@ -3,9 +3,22 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+var app = angular.module('starter', ['ionic', 'ngCordova'
+]);
 
-.run(function($ionicPlatform) {
+app.controller('CameraCtrl',function($scope, $cordovaCapture){
+  $scope.captureVideo = function () {
+    var options = {limit:1, duration:1};
+
+    $cordovaCapture.captureVideo(options).then(function (videoData) {
+
+      }, function(err) {
+
+      });
+  };
+});
+
+app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
@@ -21,4 +34,5 @@ angular.module('starter', ['ionic'])
       StatusBar.styleDefault();
     }
   });
+
 })
